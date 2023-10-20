2247262f60f2664d867078fac742c9166e004062
Sean Behan <codebam@riseup.net>
2023-10-20 01:06:06 -0400

    terminal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ReactTerminal } from "react-terminal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const commands = {
+		echo: (...args) => args.join(" "),
+		ls: () => fetch("https://p.seanbehan.ca/list").then((res) => res.text()),
+	};
+	return <ReactTerminal id="terminal" commands={commands} theme={"dracula"} />;
 }
 
 export default App;
