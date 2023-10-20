414e325b3058b0cf4b2de1af060e2b2106135830
Sean Behan <codebam@riseup.net>
2023-10-20 02:15:21 -0400

    paste files

--- src/App.js
+++ src/App.js
@@ -5,17 +5,40 @@ function App() {
 	const commands = {
 		echo: (...args) => args.join(" "),
 		ls: () => fetch("https://p.seanbehan.ca/list").then((res) => res.text()),
-		paste: (...args) =>
-			fetch("https://p.seanbehan.ca", {
-				method: "POST",
-				body: args.join(" "),
-			}).then((res) => res.text()),
+		paste: async (...args) => {
+			let body = "";
+			if (args[0] !== "") {
+				body = args.join(" ");
+				return await fetch("https://p.seanbehan.ca", {
+					method: "POST",
+					body,
+				}).then((res) => res.text());
+			} else {
+				const dialog = document.createElement("dialog");
+				const textarea = document.createElement("textarea");
+				textarea.placeholder = "Hit shift+enter when done";
+				textarea.addEventListener("keyup", async (event) => {
+					if (
+						event.shiftKey &&
+						(event.keyCode === 13 || event.key === "Enter")
+					) {
+						dialog.remove();
+						body = textarea.value;
+						await fetch("https://p.seanbehan.ca", {
+							method: "POST",
+							body,
+						}).then((res) => res.text());
+					}
+				});
+				dialog.appendChild(textarea);
+				document.body.appendChild(dialog);
+				dialog.show();
+			}
+		},
 	};
 	return (
 		<ReactTerminal
-			id="terminal"
 			prompt={"$"}
-			welcomeMessage={"Sean's Pastebin"}
 			showControlButtons={false}
 			showControlBar={false}
 			commands={commands}
